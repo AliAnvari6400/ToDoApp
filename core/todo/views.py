b12ece91f706a62f531c80d88b8bd14c4f0f849e
ALi <a_anvari@ymail.com>
2025-08-02 23:39:43 +0330

    loginrequiredmixin added to todo views

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,27 +1,35 @@
 from django.views.generic import CreateView,ListView,UpdateView,DeleteView
 from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+# customize LoginRequiredMixin for redirect to login page first
+class MyLoginRequiredMixin(LoginRequiredMixin):
+    def get_login_url(self):
+        return reverse_lazy('accounts:login')
+
 
 # Create,list,edit,delete and complete view for tasks
-class TaskCreateView(CreateView):
+class TaskCreateView(MyLoginRequiredMixin,CreateView):
     model = Task
     fields = ['author','title']
     success_url = '/todo/task/'
     
-class TaskListView(ListView):
+class TaskListView(MyLoginRequiredMixin,ListView):
     model = Task
     paginate_by = 100
     context_object_name = 'tasks'
     
-class TaskEditView(UpdateView):
+class TaskEditView(MyLoginRequiredMixin,UpdateView):
     model = Task
     fields = ['author','title']
     success_url = '/todo/task/'
     
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(MyLoginRequiredMixin,DeleteView):
     model = Task
     success_url = '/todo/task/'
 
-class TaskCompleteView(UpdateView):
+class TaskCompleteView(MyLoginRequiredMixin,UpdateView):
     model = Task
     fields = ['status']
     success_url = '/todo/task/'
