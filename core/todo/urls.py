e924cb6228a7027797683c5096ae5558222d792f
ALi <a_anvari@ymail.com>
2025-08-06 18:49:03 +0330

    set template for app

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import TaskCreateView, TaskListView,TaskEditView,TaskDeleteView,TaskCompleteView
+from .views import TaskCreateView, TaskListView,TaskEditView,TaskDeleteView,TaskCompleteView,TaskCombinedView
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('task/create/', TaskCreateView.as_view(), name = 'task-create'),
-    path('task/', TaskListView.as_view(), name = 'task-list'),
+    path('task/', TaskCombinedView.as_view(), name = 'task-create-list'),
+    #path('task/', TaskListView.as_view(), name = 'task-list'),
     path('task/<int:pk>/edit/', TaskEditView.as_view(), name = 'task-edit'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name = 'task-delete'),
     path('task/<int:pk>/complete/', TaskCompleteView.as_view(), name = 'task-complete'),
--- core/todo/views.py
+++ core/todo/views.py
@@ -2,6 +2,7 @@ from django.views.generic import CreateView,ListView,UpdateView,DeleteView
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from .forms import TaskForm
 
 # customize LoginRequiredMixin for redirect to login page first
 class MyLoginRequiredMixin(LoginRequiredMixin):
@@ -10,15 +11,26 @@ class MyLoginRequiredMixin(LoginRequiredMixin):
 
 
 # Create,list,edit,delete and complete view for tasks
-class TaskCreateView(MyLoginRequiredMixin,CreateView):
+class TaskCombinedView(MyLoginRequiredMixin,CreateView,ListView):
     model = Task
-    fields = ['author','title']
+    form_class = TaskForm
+    #fields = ['author','title']
     success_url = '/todo/task/'
-    
-class TaskListView(MyLoginRequiredMixin,ListView):
-    model = Task
-    paginate_by = 100
+    template_name = 'todo/task.html'
+    ordering = '-created_date'
     context_object_name = 'tasks'
+    paginate_by = 10
+    
+    def form_valid(self,form):
+        form.instance.author.user = self.request.user
+        return super().form_valid(form)
+
+# class TaskListView(MyLoginRequiredMixin,ListView):
+#     model = Task
+#     paginate_by = 100
+#     context_object_name = 'tasks'
+#     template_name = 'todo/task.html'
+#     ordering = '-created_date'
     
 class TaskEditView(MyLoginRequiredMixin,UpdateView):
     model = Task
