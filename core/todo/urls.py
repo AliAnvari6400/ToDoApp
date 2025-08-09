fb525071294f80c82e2b888d8fae08e9f2b93d5c
ALi <a_anvari@ymail.com>
2025-08-09 21:52:45 +0330

    createview and listview setup

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskEditView,TaskDeleteView,TaskCompleteView,TaskView
+from .views import TaskEditView,TaskDeleteView,TaskCompleteView,TaskListView,TaskCreateView
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('task/', TaskView.as_view(), name = 'task'),
+    path('task/', TaskListView.as_view(), name = 'task-list'),
+    path('task/create/', TaskCreateView.as_view(), name = 'task-create'),
     #path('task/', TaskListView.as_view(), name = 'task-list'),
     path('task/<int:pk>/edit/', TaskEditView.as_view(), name = 'task-edit'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name = 'task-delete'),
--- core/todo/views.py
+++ core/todo/views.py
@@ -9,25 +9,28 @@ class MyLoginRequiredMixin(LoginRequiredMixin):
     def get_login_url(self):
         return reverse_lazy('accounts:login')
 
-
 # Create,list,edit,delete and complete view for tasks
-
-class TaskView(MyLoginRequiredMixin,CreateView,ListView):
-    model = Task
+class TaskCreateView(MyLoginRequiredMixin,CreateView):
+    model = Task 
     #form_class = TaskForm
     fields = ['author','title']
     success_url = '/todo/task/'
-    template_name = 'todo/task.html'
-    context_object_name = 'tasks'
-    paginate_by = 50
-    ordering = '-created_date'
-        
+    # template_name = 'todo/task.html'
+    
     def form_valid(self,form):
         instance = form.save(commit=False) 
-        instance.author.user = self.request.user
+        form.instance.author.user = self.request.user
         instance.save()
         return super().form_valid(form)
     
+class TaskListView(MyLoginRequiredMixin,ListView):
+    model = Task
+    template_name = 'todo/task.html'
+    success_url = '/todo/task/'
+    context_object_name = 'tasks'
+    paginate_by = 50
+    ordering = '-created_date' 
+      
     def get_queryset(self):
         queryset = super().get_queryset()
         current_user = self.request.user
