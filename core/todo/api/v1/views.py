ed5c5f1db627210c9e093dd4073aaffbf10af3a0
ALi <a_anvari@ymail.com>
2025-09-05 01:31:18 +0330

    added permissions,paginations,filters in drf

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -1,9 +1,19 @@
 from .serializers import TaskSerializer
 from ...models import Task
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from .paginations import DefaultPagination
 
 class TaskModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,IsOwnerOrReadOnly]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author','status']  
+    search_fields = ['title']      
+    ordering_fields = ['published_date']
+    
+    pagination_class = DefaultPagination
