b591ce27a19bcfe5ca393aea06b265e95769e96e
ALi <a_anvari@ymail.com>
2025-10-15 13:11:49 +0330

    completed load testing locust with jwt in header

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -2,6 +2,17 @@ from locust import HttpUser, task
 
 
 class task_list(HttpUser):
+    
+    def on_start(self):
+        response = self.client.post('/accounts/api/v1/jwt/create/', data = {
+            'email':'a_anvari@ymail.com',
+            'password':'anvari@7768'
+        }).json()
+        
+        self.client.headers.update({
+            'Authorization':f'Bearer {response.get('access',None)}'
+        })
+    
     @task
-    def hello_world(self):
+    def task(self):
         self.client.get("/todo/api/v1/task/")
