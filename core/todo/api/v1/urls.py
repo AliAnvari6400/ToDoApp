6c364ce4faa479574c8229058091c0082cdd3b10
ALi <a_anvari@ymail.com>
2025-10-26 18:56:43 +0330

    undo changing in urls of DefaultRouter

--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from v1.views import TaskModelViewSet, WeatherAPIView
+from .views import TaskModelViewSet, WeatherAPIView
 
 app_name = "api-v1"
 
 router = DefaultRouter()
-router.register(r"task", TaskModelViewSet, basename="task")
+router.register("task", TaskModelViewSet, basename="task")
 
 urlpatterns = [
     path("", include(router.urls)),
